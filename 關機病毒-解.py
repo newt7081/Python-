6e0ca14177650925fc0ada3a1ef86e4e2d7c2bcd
newt7081 <89738404+newt7081@users.noreply.github.com>
2021-09-13 15:43:38 +0800

    Add files via upload

---
+++ 關機病毒-解.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+import os
+from tkinter import messagebox
+# 主程式-------------------------------------------------------->
+root = tk.Tk()
+root.withdraw()
+os.system("shutdown -a")
